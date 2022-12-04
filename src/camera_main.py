d9fa53e84875299866a9a65ecb9d23a1b1287b7f
Tomi Tuhkanen <tomi.tuhkanen@iki.fi>
2022-12-04 21:44:06 +0200

    Combine camera event listener to a single thread

--- src/camera_main.py
+++ src/camera_main.py
@@ -3,10 +3,10 @@ import socket
 import time
 from threading import Thread
 
+import data_store
+import event_handler
+import video_stream_producer
 from common_types import CameraStatus, EventType, VideoFrame
-from data_store import update_camera_address, update_camera_status
-from event_handler import wait_for_events
-from video_stream_producer import send_frame, try_init_socket
 
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument("--camera-id", dest="camera_id", default=0)
@@ -23,7 +23,6 @@ RUN_CAMERA = RunFlag()
 STREAM_CAMERA = RunFlag()
 STREAM_CAMERA.running = True
 RECORD_CAMERA = RunFlag()
-RUN_RECORD_CHECK = RunFlag()
 
 
 # TODO: 2 ids are required. One system wide id and one for device camera id.
@@ -40,35 +39,36 @@ def _recording_on(camera_id: int) -> bool:
     return RECORD_CAMERA.running
 
 
-def _check_camera_on(camera_id: int):
+def _listen_camera_events(camera_id: int):
     while True:
-        for event, _ in wait_for_events([EventType.CAMERA_PREPARE, EventType.CAMERA_TURNOFF], camera_id):
+        for event, _ in event_handler.wait_for_events(
+            [EventType.CAMERA_PREPARE, EventType.CAMERA_TURNOFF, EventType.CAMERA_RECORD, EventType.CAMERA_STOP_RECORD],
+            camera_id,
+        ):
             # should_run = get_camera_running(camera_id)
-            RUN_CAMERA.running = event == EventType.CAMERA_PREPARE.value
-
-
-def _check_recording_on(camera_id: int):
-    while RUN_RECORD_CHECK.running:
-        for event, _ in wait_for_events([EventType.CAMERA_RECORD, EventType.CAMERA_STOP_RECORD], camera_id):
-            # should_record = get_camera_recording(camera_id)
-            RECORD_CAMERA.running = event == EventType.CAMERA_RECORD.value
+            if event in [EventType.CAMERA_PREPARE.value, EventType.CAMERA_TURNOFF.value]:
+                RUN_CAMERA.running = event == EventType.CAMERA_PREPARE.value
+                data_store.update_camera_running(camera_id, RUN_CAMERA.running)
+            elif event in [EventType.CAMERA_RECORD.value, EventType.CAMERA_STOP_RECORD.value]:
+                RECORD_CAMERA.running = event == EventType.CAMERA_RECORD.value
+                data_store.update_camera_recording(camera_id, RECORD_CAMERA.running)
 
 
 def _send_status(camera_id: int, status: CameraStatus):
-    update_camera_status(camera_id, status)
+    data_store.update_camera_status(camera_id, status)
     print("Sending status:", {camera_id, status.name})
 
 
 def _update_address_info(camera_id: int, socket: socket.socket | None):
     local_address = socket.getsockname() if socket else None
     address = f"{local_address[0]}:{local_address[1]}" if local_address else None
-    update_camera_address(camera_id, address)
+    data_store.update_camera_address(camera_id, address)
     print("Update address", {"camera_id": camera_id, "address": address})
 
 
 def _new_frame_received(socket: socket.socket | None, frame: VideoFrame):
     if STREAM_CAMERA.running and socket:
-        send_frame(socket, frame)
+        video_stream_producer.send_frame(socket, frame)
         # dispaly_show_frame(frame)
 
 
@@ -87,8 +87,8 @@ def main_loop(camera_id: int, use_dummy_mode: bool):
     print("Starting:", {"camera_id": camera_id, "use_dummy_mode": use_dummy_mode})
     prepare_camera, run_camera_loop, shutdown_camera = _get_camera_functions(use_dummy_mode)
 
-    start_thread = Thread(target=_check_camera_on, args=[camera_id], daemon=True)
-    start_thread.start()
+    event_thread = Thread(target=_listen_camera_events, args=[camera_id], daemon=True)
+    event_thread.start()
 
     socket = None
 
@@ -105,16 +105,12 @@ def main_loop(camera_id: int, use_dummy_mode: bool):
         _send_status(camera_id, CameraStatus.CAMERA_PREPARE)
 
         if not socket:
-            socket = try_init_socket()
+            socket = video_stream_producer.try_init_socket()
             _update_address_info(camera_id, socket)
 
         video_capture = prepare_camera(camera_id)
         print("camera ready", {"camera_id": camera_id})
         _send_status(camera_id, CameraStatus.CAMERA_READY)
-
-        RUN_RECORD_CHECK.running = True
-        record_thread = Thread(target=_check_recording_on, args=[camera_id], daemon=True)
-        record_thread.start()
 
         run_camera_loop(
             video_capture,
@@ -124,9 +120,6 @@ def main_loop(camera_id: int, use_dummy_mode: bool):
             lambda frame: _new_frame_received(socket, frame),
         )
         shutdown_camera(video_capture)
-
-        RUN_RECORD_CHECK.running = False
-        record_thread.join()
 
         _send_status(camera_id, CameraStatus.SYSTEM_STANDBY)
 
