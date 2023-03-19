5ba1c458ccc0bf29e5ce09ccdb3d0594dbcdbba2
Tomi Tuhkanen <tomi.tuhkanen@iki.fi>
2023-03-19 18:37:50 +0200

    Add video player functionality

--- multi-camera-ui/src/components/VideoPlayer.tsx
+++ multi-camera-ui/src/components/VideoPlayer.tsx
@@ -1,22 +1,40 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import * as api from "../api";
 import { VideoFile } from "../types";
 
 const VideoPlayer = () => {
+  const videoRef = useRef();
   const [files, setfiles] = useState<VideoFile[]>();
+  const [selectedVideoUrl, setSelectedVideoUrl] = useState<string>();
 
   useEffect(() => {
-    api.getVideoFiles().then((resp) => setfiles(resp));
+    api.getVideoFiles().then((videoFiles) => {
+      setfiles(videoFiles);
+      if (videoFiles.length > 0) setSelectedVideoUrl(`/api/video/${videoFiles[0].name}`);
+    });
   }, []);
+
+  const setVideoUrl = (e: any) => {
+    setSelectedVideoUrl(`/api/video/${e.target.value}`);
+  };
+
+  useEffect(() => {
+    videoRef.current?.load();
+  }, [selectedVideoUrl]);
 
   return (
     <>
       <h1>Video Player</h1>
-      <select>
+      <select onChange={setVideoUrl}>
         {files?.map((file) => (
-          <option>{file.name}</option>
+          <option value={file.name}>{file.title}</option>
         ))}
       </select>
+      <div>
+        <video ref={videoRef} width="360" controls>
+          <source src={selectedVideoUrl} type="video/mp4" />
+        </video>
+      </div>
     </>
   );
 };
