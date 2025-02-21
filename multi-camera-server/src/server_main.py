9485d189ca8f2d9cbfc689c0ef1f3a99a7ce9596
ttu <tomi.tuhkanen@iki.fi>
2025-02-21 08:33:41 +0200

    Fix state validation in control-camera endpoint

--- multi-camera-server/src/server_main.py
+++ multi-camera-server/src/server_main.py
@@ -159,13 +159,14 @@ async def control_camera(request: Request):
     data = await request.json()
     camera_id = data["cameraId"]
     state = data["state"]
-    event = EventType(state)
+
+    try:
+        event = EventType(state)
+    except ValueError:
+        return JSONResponse(content={}, status_code=status.HTTP_400_BAD_REQUEST)
 
     if not server_core.has_camera(server_core.ROUTE_INFOS, int(camera_id)):
         return JSONResponse(content={}, status_code=status.HTTP_404_NOT_FOUND)
-
-    if not event:
-        return JSONResponse(content={}, status_code=status.HTTP_400_BAD_REQUEST)
 
     _ = event_handler.send_event(event, camera_id)
 
